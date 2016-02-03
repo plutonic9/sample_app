f6e0c2b26660401c4ec5178de1dbc73f1894bb72
John Pendergrass <plutonic9@gmail.com>
2016-02-03 00:25:13 +0000

    chap 5.3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  
   def home
   end
 
@@ -7,5 +8,8 @@ class StaticPagesController < ApplicationController
   
   def about
   end
-  
+
+  def contact
+  end
+
 end
