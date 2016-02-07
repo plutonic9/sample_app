3c8ca1747369cc8f15e1d2941a9e31bc462c0a24
John Pendergrass <plutonic9@gmail.com>
2016-02-07 20:15:56 +0000

    Add account activations - starting 10.2 - working good

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,11 +36,25 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(digest).is_password?(token)
   end  
 
-  
-  # forgets a user
+    # forgets a user
   def forget
       update_attribute(:remember_digest, nil)
   end
+
+
+
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+
   
 private
 
