c64f7c48cb99734068b9f4670124d5e6e807d5f0
John Pendergrass <plutonic9@gmail.com>
2016-02-09 22:50:26 +0000

    Finished chapter 11 - working fine

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,15 @@
 class StaticPagesController < ApplicationController
-  
+
   def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
   end
-  
+
   def about
   end
 
@@ -13,3 +17,4 @@ class StaticPagesController < ApplicationController
   end
 
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -54,3 +54,9 @@ User.create!(name:  "Tristan Pendergrass",
 end
 
 
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
