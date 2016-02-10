9d7777d4075eb67d29bca1b0e52af67e46a19e2b
John Pendergrass <plutonic9@gmail.com>
2016-02-10 18:55:33 +0000

    add user following - finished chapter 12

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,7 +41,7 @@ User.create!(name:  "Tristan Pendergrass",
              activated_at: Time.zone.now)
 
 
-30.times do |n|
+5.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -53,10 +53,17 @@ User.create!(name:  "Tristan Pendergrass",
               activated_at: Time.zone.now)
 end
 
-
+# Microposts
 users = User.order(:created_at).take(6)
-50.times do
+10.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
 
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..10]
+followers = users[3..12]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
